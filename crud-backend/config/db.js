bf8bfbc80e7bac37679e7cccba91da292e0f09f1
Abdullah Usman <abdullahusman126@gmail.com>
2022-06-28 11:28:25 +0500

    database connected

--- crud-backend/config/db.js
+++ crud-backend/config/db.js
@@ -4,9 +4,9 @@ const env = require("./envConfig")
 const connect = async() =>{
     try {
      await  mongoose.connect(env.MONGO_URL);
-     mongoose.connection.on("connected", () => console.log("mongoose connected"));
+     console.log("database connected");
     } catch (error) {
-        mongoose.connection.on("error", (error) => console.log(error));
+       console.log(error.message) ;
     }
 };
 
